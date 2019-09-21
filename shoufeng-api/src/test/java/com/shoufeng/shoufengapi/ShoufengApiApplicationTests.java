c77ddb3903cdcc58fe965afef83919769516befe
maozhihao <maozhihao@ddicar.com>
2019-09-21 15:49:41 +0800

    初始化项目，添加api、message、model、server等模块

---
+++ shoufeng-api/src/test/java/com/shoufeng/shoufengapi/ShoufengApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shoufeng.shoufengapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShoufengApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
